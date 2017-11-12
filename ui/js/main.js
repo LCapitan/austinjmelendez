0189d54df30eef9d07cb6609179a09ab8587569a
Austin Melendez <Austin.Melendez@theprimacy.com>
2017-11-12 15:49:44 -0500

    update code

--- ui/js/main.js
+++ ui/js/main.js
@@ -5,8 +5,18 @@
 $(document).ready(
 
     function main() {
-        console.log('In progress');
+        // console.log('In progress');
+        var $navbutton = $('#navBtn');
+        var $navbar = $('.nav');
+        console.log($navbar);
 
+        $navbutton.on('click', function() {
+            var $this = $(this);
+            $navbar.toggleClass('closed');
+            $this.toggleClass('open');
+            $this.toggleClass('icon-arrow-down2');
+            $this.toggleClass('icon-arrow-up2');
+        })
     }
 
 );
