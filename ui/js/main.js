6246df3fa0548649e83a3f5110f8ecf5a19e0b54
Austin Melendez <Austin.Melendez@theprimacy.com>
2017-11-12 17:02:50 -0500

    update front end 2

--- ui/js/main.js
+++ ui/js/main.js
@@ -6,16 +6,13 @@ $(document).ready(
 
     function main() {
         // console.log('In progress');
-        var $navbutton = $('#navBtn');
-        var $navbar = $('.nav');
-        console.log($navbar);
 
-        $navbutton.on('click', function() {
-            var $this = $(this);
-            $navbar.toggleClass('closed');
-            $this.toggleClass('open');
-            $this.toggleClass('icon-arrow-down2');
-            $this.toggleClass('icon-arrow-up2');
+        $nav = $('.nav');
+        $navButton = $('#navBtn');
+
+
+        $navButton.on('click', function() {
+            $nav.toggleClass('closed');
         })
     }
 
